709d735cb7fbfe82cd027d48dcbdc284448c9ec0
oakleyKatt <okatterheinrich@gmail.com>
2017-04-30 21:43:25 +0000

    rpi implementation test

---
+++ rpi-pulse/serial_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import serial
+import struct
+
+ser = serial.Serial('/dev/ttyACM0',9600)
+s = [0]
+while 1:
+    read_serial = ser.readline()
+#    s[0] = struct.unpack("h",read_serial)
+    s[0] = str(int(ser.readline(),16))
+    print s[0]
+    print read_serial
